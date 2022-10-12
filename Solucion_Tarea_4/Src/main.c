f5210695d8f242c6d7cf7ceb3db7261fde4e7243
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2022-10-12 17:19:25 -0500

    Configuracion del EXTI  y comienzo del main

--- Solucion_Tarea_4/Src/main.c
+++ Solucion_Tarea_4/Src/main.c
@@ -13,6 +13,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+uint8_t flag1 = 0;
+uint8_t flag2 = 0;
+uint8_t buffer[8];
+uint8_t counter = 0;
+uint8_t string1[];
 
 void inSystem (void);
 
@@ -28,9 +33,26 @@ BasicTimer_Handler_t handlerTIM2 = {0};
 USART_Handler_t handlerUSART2 = {0};
 
 
+
+
 int main (void){
 
+	inSystem();
+
 	while(1){
+
+		if (flag1){
+
+			uint8_t string1 = "CW direction = ";
+			writeMsg(&handlerUSART2, string1);
+			counter++;
+			sprintf(buffer, '%u \n', counter);
+			writeMsg(&handlerUSART2, buffer);
+
+
+		}else if
+
+
 
 	}
 }
@@ -89,24 +111,28 @@ void inSystem (void){
 
 	BasicTimer_Config(&handlerTIM2);
 
+	startTimer (&handlerTIM2);
+
 	handlerUSART2.ptrUSARTx = USART2;
 	handlerUSART2.USART_Config.USART_baudrate = USART_BAUDRATE_115200;
 	handlerUSART2.USART_Config.USART_datasize = USART_DATASIZE_8BIT;
-	handlerUSART2.USART_Config.USART_mode = USART_MODE_TX;
+	handlerUSART2.USART_Config.USART_mode = USART_MODE_RXTX;
 	handlerUSART2.USART_Config.USART_stopbits = USART_STOPBIT_1;
 
 	USART_Config(&handlerUSART2);
 
 
 
+
+
 }
 
 void callback_extInt0(void){
-
+	flag1 = 1;
 }
 
 void callback_extInt1(void){
-
+	flag2 = 1;
 }
 
 void BasicTimer2_Callback(void){
