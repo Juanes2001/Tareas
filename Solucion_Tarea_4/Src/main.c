517b2b24f19a28501d54c6a9b61b643260c00c42
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2022-10-11 23:13:22 -0500

    Comienzo del software tarea4
    Comence defininendo los perifericos que usare para comenzar controlando el Encoder primero.

--- Solucion_Tarea_4/Src/main.c
+++ Solucion_Tarea_4/Src/main.c
@@ -9,35 +9,106 @@
 #include "BasicTimer.h"
 #include "GPIOxDriver.h"
 #include "USARTxDriver.h"
+#include"EXTIDriver.h"
 #include <stdint.h>
 #include <stdbool.h>
 
 
 void inSystem (void);
 
-/*
- * Definimos todas las variables que almacenan la estructura de cada periferico a usar,
- * los GPIO para el LED  y el botton, y otro para usar el protocolo de comunicación USART.
- * Las variables respectivas para el Timer y también para el USART. Todas las inicializamos en 0
- */
-GPIO_Handler_t handlerUserLed = {0};
-GPIO_Handler_t handlerGPIOUSART1 = {0};
-GPIO_Handler_t handlerUserButton = {0};
+
+GPIO_Handler_t handlerBlinkyLed = {0};
+//GPIO_Handler_t handlerUserPinUsart = {0};
+//GPIO_Handler_t handlerExIn1 = {0};
+//GPIO_Handler_t handlerExIn2 ={0};
+EXTI_Config_t handlerExtiConfig0 = {0};
+EXTI_Config_t handlerExtiConfig1 = {0};
+EXTI_Config_t handlerExtiConfig2 = {0};
 BasicTimer_Handler_t handlerTIM2 = {0};
-USART_Handler_t handlerUSART1= {0};
+USART_Handler_t handlerUSART2 = {0};
 
-
-/*
- * Creamos 2 variables, una bandera que nos ayudará a usar las interrupciones, y un contador
- * que almacenará los caracteres del codigo ASCII a partir del 32 en adelante, ya que los numeros
- * anteriores almacenan funciones.
- */
-uint8_t flag1 = 0;
-uint8_t countr = 32;
 
 int main (void){
 
 	while(1){
 
 	}
+}
+
+
+void inSystem (void){
+
+	handlerBlinkyLed.pGPIOx = GPIOA;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinNumber = PIN_5;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinPuPdControl =  GPIO_PUPDR_NOTHING;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OTYPE_PUSHPULL;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OSPEEDR_FAST;
+
+	GPIO_Config(&handlerBlinkyLed);
+
+
+//	handlerExIn0.pGPIOx = GPIOA;
+//	handlerExIn0.GPIO_PinConfig.GPIO_PinNumber = PIN_0;
+//	handlerExIn0.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+//	handlerExIn0.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+//	handlerExIn0.GPIO_PinConfig.GPIO_PinPuPdControl =  GPIO_PUPDR_NOTHING;
+//	handlerExIn0.GPIO_PinConfig.GPIO_PinOPType = GPIO_OTYPE_PUSHPULL;
+//	handlerExIn0.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OSPEEDR_FAST;
+
+//	GPIO_Config(&handlerExIn0);
+
+	handlerExtiConfig0.pGPIOHandler->GPIO_PinConfig.GPIO_PinNumber = PIN_0;
+	handlerExtiConfig0.pGPIOHandler->pGPIOx = GPIOA;
+	handlerExtiConfig0.pGPIOHandler->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	handlerExtiConfig0.pGPIOHandler->GPIO_PinConfig.GPIO_PinAltFunMode =AF0;
+	handlerExtiConfig0.pGPIOHandler->GPIO_PinConfig.GPIO_PinOPType = GPIO_OTYPE_PUSHPULL;
+	handlerExtiConfig0.pGPIOHandler->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	handlerExtiConfig0.pGPIOHandler->GPIO_PinConfig.GPIO_PinSpeed = GPIO_OSPEEDR_FAST;
+	handlerExtiConfig0.edgeType = EXTERNAL_INTERRUPT_RISING_EDGE;
+
+	extInt_Config(&handlerExtiConfig0);
+
+	handlerExtiConfig1.pGPIOHandler->GPIO_PinConfig.GPIO_PinNumber       = PIN_1;
+	handlerExtiConfig1.pGPIOHandler->pGPIOx                              = GPIOA;
+	handlerExtiConfig1.pGPIOHandler->GPIO_PinConfig.GPIO_PinMode         = GPIO_MODE_IN;
+	handlerExtiConfig1.pGPIOHandler->GPIO_PinConfig.GPIO_PinAltFunMode   = AF0;
+	handlerExtiConfig1.pGPIOHandler->GPIO_PinConfig.GPIO_PinOPType       = GPIO_OTYPE_PUSHPULL;
+	handlerExtiConfig1.pGPIOHandler->GPIO_PinConfig.GPIO_PinPuPdControl  = GPIO_PUPDR_NOTHING;
+	handlerExtiConfig1.pGPIOHandler->GPIO_PinConfig.GPIO_PinSpeed        = GPIO_OSPEEDR_FAST;
+	handlerExtiConfig1.edgeType                                          = EXTERNAL_INTERRUPT_RISING_EDGE;
+
+	extInt_Config(&handlerExtiConfig0);
+
+	handlerTIM2.ptrTIMx = TIM2;
+	handlerTIM2.TIMx_Config.TIMx_mode = BTIMER_MODE_UP;
+	handlerTIM2.TIMx_Config.TIMx_speed = BTIMER_SPEED_100us;
+	handlerTIM2.TIMx_Config.TIMx_period = 2500;
+	handlerTIM2.TIMx_Config.TIMx_interruptEnable = 1;
+
+	BasicTimer_Config(&handlerTIM2);
+
+	handlerUSART2.ptrUSARTx = USART2;
+	handlerUSART2.USART_Config.USART_baudrate = USART_BAUDRATE_115200;
+	handlerUSART2.USART_Config.USART_datasize = USART_DATASIZE_8BIT;
+	handlerUSART2.USART_Config.USART_mode = USART_MODE_TX;
+	handlerUSART2.USART_Config.USART_stopbits = USART_STOPBIT_1;
+
+	USART_Config(&handlerUSART2);
+
+
+
+}
+
+void callback_extInt0(void){
+
+}
+
+void callback_extInt1(void){
+
+}
+
+void BasicTimer2_Callback(void){
+	GPIOxTooglePin(&handlerBlinkyLed);
 }
