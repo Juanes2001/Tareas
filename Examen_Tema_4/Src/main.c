18f1c31578cd9f8a2b032de4cf62bd820a8bc18b
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2022-11-02 22:47:56 -0500

    Blinky

--- Examen_Tema_4/Src/main.c
+++ Examen_Tema_4/Src/main.c
@@ -26,6 +26,9 @@ ADC_Config_t handlerADCJoy = {0};
 GPIO_Handler_t handlerPinTx = {0};
 GPIO_Handler_t handlerPinRx = {0};
 
+//Pin del blinky
+GPIO_Handler_t handlerLEDPin = {0};
+
 //handler para USART1
 USART_Handler_t handlerUSART1 = {0};
 
@@ -49,9 +52,32 @@ int main(void){
 
 void inSystem (void){
 
+	handlerTIM2.ptrTIMx = TIM2;
+	handlerTIM2.TIMx_Config.TIMx_interruptEnable = 1;
+	handlerTIM2.TIMx_Config.TIMx_mode = BTIMER_MODE_UP;
+	handlerTIM2.TIMx_Config.TIMx_period = 2500;
+	handlerTIM2.TIMx_Config.TIMx_speed = BTIMER_SPEED_100us;
+	BasicTimer_Config(&handlerTIM2);
+
+	startTimer(&handlerTIM2);
+
+	handlerLEDPin.pGPIOx = GPIOA;
+	handlerLEDPin.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	handlerLEDPin.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	handlerLEDPin.GPIO_PinConfig.GPIO_PinOPType = GPIO_OTYPE_PUSHPULL;
+	handlerLEDPin.GPIO_PinConfig.GPIO_PinNumber = PIN_5;
+	handlerLEDPin.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	handlerLEDPin.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OSPEEDR_FAST;
+	GPIO_Config(&handlerLEDPin);
+
+//	handlerADCJoy.channelVector = {ADC_CHANNEL_0, ADC_CHANNEL_1};
 
 
+}
 
+
+void BasicTimer2_Callback(void){
+	GPIOxTooglePin(&handlerLEDPin);
 }
 
 
