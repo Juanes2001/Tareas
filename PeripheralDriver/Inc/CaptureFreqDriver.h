85ff21fd8428e5a7e0e5457a34b27055f9fc1325
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2022-11-19 11:50:02 -0500

    .

---
+++ PeripheralDriver/Inc/CaptureFreqDriver.h
@@ -0,0 +1,13 @@
+/*
+ * CaptureFreqDriver.h
+ *
+ *  Created on: 19 nov. 2022
+ *      Author: JUAN ESTEBAN
+ */
+
+#ifndef CAPTUREFREQDRIVER_H_
+#define CAPTUREFREQDRIVER_H_
+
+
+
+#endif /* CAPTUREFREQDRIVER_H_ */
---
+++ PeripheralDriver/Src/CaptureFreqDriver.c
@@ -0,0 +1,8 @@
+/*
+ * CaptureFreqDriver.c
+ *
+ *  Created on: 19 nov. 2022
+ *      Author: JUAN ESTEBAN
+ */
+
+
